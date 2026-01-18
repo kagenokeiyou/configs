073307268b9e855de91df749880fff49e2a7cdd1
kagenokeiyou <kagenokeiyou@outlook.com>
2026-01-18 16:47:17 +0800

    PowerShell: update

--- configs/powershell/Microsoft.PowerShell_profile.ps1
+++ configs/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,21 @@
 Set-PSReadLineOption -PredictionViewStyle ListView
 
 #region starship
-Invoke-Expression (&starship init powershell)
+Invoke-Expression (& starship init powershell)
+#endregion
+
+#region alias
+Set-Alias -Name ls -Value lsd -Force
+
+function la {
+    & lsd -a
+}
+
+function ll {
+    & lsd -al
+}
+
+function lt {
+    & lsd --tree
+}
 #endregion
--- configs/powershell/profile.ps1
+++ configs/powershell/profile.ps1
@@ -3,10 +3,7 @@ $Script:CondaExecutable = "D:\ProgramData\anaconda3\Scripts\conda.exe"
 function conda {
     if (-not $Script:CondaInitialized) {
         if (Test-Path $Script:CondaExecutable) {
-            (& $Script:CondaExecutable "shell.powershell" "hook") |
-            Out-String |
-            Where-Object { $_ } |
-            Invoke-Expression
+            (& $Script:CondaExecutable "shell.powershell" "hook") | Out-String | Where-Object { $_ } | Invoke-Expression
             $Script:CondaInitialized = $true
         }
     }
@@ -18,6 +15,6 @@ return # skip raw conda initialize
 #region conda initialize
 # !! Contents within this block are managed by 'conda init' !!
 If (Test-Path "D:\ProgramData\anaconda3\Scripts\conda.exe") {
-    (& "D:\ProgramData\anaconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
+    (& "D:\ProgramData\anaconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ? { $_ } | Invoke-Expression
 }
 #endregion
